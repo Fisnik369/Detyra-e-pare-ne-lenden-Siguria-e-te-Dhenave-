9bc4b42849c91ec2a1d3670ddff55e3b55284fcf
Fisnik369 <59198126+Fisnik369@users.noreply.github.com>
2020-06-07 22:24:51 +0200

    Update FazaETrete.java

--- FazaETrete.java
+++ FazaETrete.java
@@ -60,6 +60,14 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
+import javax.xml.bind.DatatypeConverter;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Claims;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import io.jsonwebtoken.*;
+import java.util.Date;
 
 
 public class RsaKey{
@@ -241,6 +249,15 @@ public class RsaKey{
 			
 					
 		}
+	    else if(paraqit.contains("generate-token")){
+	    
+	    String user = args[1];
+	    String subject = args[2];
+		    
+	    System.out.println(createJWT("11", user, subject, 60*60*20));	    
+	    
+	    
+	    }
 		
 		
     }
@@ -725,6 +742,56 @@ public class RsaKey{
 		
 		
 	}
+	
+	private static String createJWT(String id, String issuer, String subject, long ttlMillis) {
+		 
+	    //The JWT signature algorithm we will be using to sign the token
+	    SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+	 
+	    long nowMillis = System.currentTimeMillis();
+	    Date now = new Date(nowMillis);
+	 
+	    //We will sign our JWT with our ApiKey secret
+	    byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(apiKey.getSecret());
+	    Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+	 
+	    //Let's set the JWT Claims
+	    JwtBuilder builder = Jwts.builder().setId(id)
+	                                .setIssuedAt(now)
+	                                .setSubject(subject)
+	                                .setIssuer(issuer)
+	                                .signWith(signatureAlgorithm, signingKey);
+	 
+	    //if it has been specified, let's add the expiration
+	    if (ttlMillis >= 0) {
+	    long expMillis = nowMillis + ttlMillis;
+	        Date exp = new Date(expMillis);
+	        builder.setExpiration(exp);
+	    }
+	 
+	    //Builds the JWT and serializes it to a compact, URL-safe string
+	    return builder.compact();
+	}
+	
+	
+	
+	private void parseJWT(String jwt) {
+		 
+	    //This line will throw an exception if it is not a signed JWS (as expected)
+	    Claims claims = Jwts.parser()         
+	       .setSigningKey(DatatypeConverter.parseBase64Binary(apiKey.getSecret()))
+	       .parseClaimsJws(jwt).getBody();
+	    System.out.println("ID: " + claims.getId());
+	    System.out.println("Subject: " + claims.getSubject());
+	    System.out.println("Issuer: " + claims.getIssuer());
+	    System.out.println("Expiration: " + claims.getExpiration());
+	    
+	    
+	}
+
+
+	
+	
 	
     }
 
